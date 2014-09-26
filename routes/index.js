122260b129c5d3d9dec2f489b57ed1d3fa602f11
Evan Senter <evansenter@gmail.com>
2014-09-26 13:34:39 -0400

    REPL config
    Signed-off-by: Evan Senter <evansenter@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require("express");
-var router = express.Router();
+var router  = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res) {
   res.render("index", { title: "Express" });
+});
+
+router.get("/pony", function(req, res) {
+  res.render("index", { title: "Ponyz" });
 });
 
 module.exports = router;
