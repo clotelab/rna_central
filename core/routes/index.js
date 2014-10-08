d9927c27ac9f71e854aa794e7da69d9281813be7
Evan Senter <evansenter@gmail.com>
2014-10-08 15:15:21 -0400

    Subclassing Repl-it with support for blacklisting packages you don't want loaded, now that I can better hook into its functions.

--- core/routes/index.js
+++ core/routes/index.js
@@ -9,12 +9,6 @@ var base_router = exports = module.exports = function(options) {
   router.__proto__ = express.Router();
   
   __.each(methods, __.bind(function(method) {
-    this.__proto__["_base_" + method] = __.wrap(this.__proto__.__proto__[method], function(wrapped_verb) {
-      var response              = wrapped_verb.apply(this, __.toArray(arguments).slice(1));
-      __.last(this.stack)._core = true;
-      return response;
-    });
-    
     this[method] = __.wrap(this.__proto__.__proto__[method], function(wrapped_verb) {
       var response                     = wrapped_verb.apply(this, __.toArray(arguments).slice(1));
       var top_core_index_in_middleware = __.indexOf(__.pluck(router.stack, "_core"), true);
@@ -31,21 +25,23 @@ var base_router = exports = module.exports = function(options) {
   router.update_settings(function() {
     this.webserver_for = "Core";
   
-    this._base_get(["/", "/home"], __.bind(function(req, res) {
+    this.get(["/", "/home"], __.bind(function(req, res) {
       res.render("index", { title: this.webserver_for + "::Home" });
     }, this));
 
-    this._base_get("/server", __.bind(function(req, res) {
+    this.get("/server", __.bind(function(req, res) {
       res.render("index", { title: this.webserver_for + "::Server" });
     }, this));
 
-    this._base_get("/info", __.bind(function(req, res) {
+    this.get("/info", __.bind(function(req, res) {
       res.render("index", { title: this.webserver_for + "::Info" });
     }, this));
 
-    this._base_get("/contact", __.bind(function(req, res) {
+    this.get("/contact", __.bind(function(req, res) {
       res.render("index", { title: this.webserver_for + "::Contact" });
     }, this));
+    
+    __.each(this.stack, function(layer) { layer._core = true; });
   });
   
   return router;
--- core/utils/repl_plus/repl.js
+++ core/utils/repl_plus/repl.js
@@ -2,8 +2,28 @@
 'use strict';
 
 var __        = require("underscore");
+var fs        = require("fs");
+var path      = require("path");
 var replit    = require("repl-it");
 var highlight = require("ansi-highlight");
+
+try {
+  var ignoreList = JSON.parse(fs.readFileSync(path.join(__dirname, "ignore.json")));
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
+
+replit.prototype.loadPackages = __.wrap(replit.prototype.loadPackages, function(loadPackages) {
+  var args = __.toArray(arguments).slice(1)
+  args.splice(1, 0, __.difference(args.splice(1, 1)[0], ignoreList));
+  
+  try {
+    loadPackages.apply(this, args);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 var repl = exports = module.exports = function() {
   global.__   = __;
