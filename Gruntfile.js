9fd08316a7bda738b03392201d114a877aa68092
Evan Senter <evansenter@gmail.com>
2015-03-04 18:43:34 -0500

    grunt-wiredep was being a pain with relative paths, removed it
    Signed-off-by: Evan Senter <evansenter@gmail.com>

--- Gruntfile.js
+++ Gruntfile.js
@@ -33,13 +33,12 @@ module.exports = function(grunt) {
     },
     shell: {
       bootstrap: {
-        command: "rm -r node_modules && rm -r bower_components && npm-check-updates -u && npm install && bower install && grunt wiredep"
+        command: "rm -r node_modules && rm -r lib/public/vendor && npm-check-updates -u && npm install && bower install"
       }
     }
   });
   
   grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks("grunt-wiredep");
   grunt.loadNpmTasks("grunt-mocha-test");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("grunt-shell");
