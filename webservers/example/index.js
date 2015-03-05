33df74bfbe7f4607c644c3fc2f3d8bbf8c4fab93
Evan Senter <evansenter@gmail.com>
2015-03-05 17:50:28 -0500

    Factored out form_builder code injection in routes, session support
    Signed-off-by: Evan Senter <evansenter@gmail.com>

--- webservers/example/index.js
+++ webservers/example/index.js
@@ -11,23 +11,31 @@ var webserver   = module.exports = base_router({
   // The title key is the pretty name for the tab, the path string / array are the subpaths that point to this tab and the template
   // key is a path that points to the HTML file for the tab. Files are looked up relative to the current directory, or in lib/views
   tabs: [
-    { title: "Server", path: "/submit_job", template: "server", meta: "server" },
+    { title: "Server", path: "/submit_job", template: "server", meta: "form" },
     { title: "Extra", path: "/extra", template: "index" },
     { title: "Another", path: ["/about", "/about2"], template: "./views/about" }
   ]
 });
 
-webserver.form_builder(function(fields, validators) {
+webserver.form_builder(function(fields, validators, widgets) {
   return {
-    email: fields.email({ attrs: { placeholder: "email@example.com" } }),
-    rna_sequence: fields.string({ required: true, attrs: { placeholder: "GGGGCCCCAAAAUAUA" } })
+    email: fields.email({ 
+      required: true,
+      cssClasses: { field: ["pure-control-group"] },
+      widget: widgets.text({ placeholder: "email@example.com", required: "true" } )
+    }),
+    rna_sequence: fields.string({
+      required: true,
+      cssClasses: { field: ["pure-control-group"] },
+      widget: widgets.text({ placeholder: "GGGGCCCCAAAAUAUA", required: "true" } )
+    })
   };
 });
 
-// webserver.validate_form(function() {
-//
-// });
-//
+webserver.form_validator(function(form, flash) {
+  flash("Shit is fucked.");
+});
+
 // webserver.generate_command(function() {
 //
 // });
